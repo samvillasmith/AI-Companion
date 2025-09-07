ce13bdf1a626287bda46d509375fe2e0ebc7c08d
samvillasmith <samuel.villa.smith@hotmail.com>
2025-09-07 00:02:09 -0500

    forcing hard reloads every render to reset layout

--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Button } from "./ui/button";
 import { Sparkles } from "lucide-react";
-import { toast } from "sonner"
+import { toast } from "sonner";
 
 interface SubscriptionButtonProps {
     isPremium: boolean;
@@ -15,19 +15,22 @@ export const SubscriptionButton = ({
     isPremium = false
 }: SubscriptionButtonProps) => {
     const [loading, setLoading] = useState(false);
+    
     const onClick = async () => {
         try {
             setLoading(true);
             const response = await axios.get("/api/stripe");
             window.location.href = response.data.url;
         } catch (error) {
+            toast.error("Something went wrong");
             setLoading(false);
         } 
     }
+    
     return(
         <Button disabled={loading} onClick={onClick} size="sm" variant={isPremium ? "default": "premium"}>
-        {isPremium ? "Manage Subscription" : "Upgrade"}
-        {isPremium && <Sparkles className="h-4 ml-2 fill-white"/>}
-    </Button>
+            {isPremium ? "Manage Subscription" : "Upgrade"}
+            {!isPremium && <Sparkles className="h-4 ml-2 fill-white"/>}
+        </Button>
     )  
 }
